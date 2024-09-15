c9f92fd7cdbc0e2b8ef9e07533faf81f61d24183
Ganesh V <ganeshv@Ganeshs-MacBook-Pro.local>
2024-09-15 17:47:12 +0530

    Linking the context to state

--- ContextAPI-useReducer/src/App.jsx
+++ ContextAPI-useReducer/src/App.jsx
@@ -67,8 +67,13 @@ function App() {
     });
   }
 
+  const initialContext ={
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart
+  }
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={initialContext}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -76,7 +81,7 @@ function App() {
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product}/>
           </li>
         ))}
       </Shop>
