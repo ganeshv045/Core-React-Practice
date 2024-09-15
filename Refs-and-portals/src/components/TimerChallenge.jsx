5116fe6b93ef5c2bcb6dcb4f15d74e9e982a8041
Ganesh V <ganeshv@Ganeshs-MacBook-Pro.local>
2024-09-15 12:44:23 +0530

    Using refs not only for DOM connections but for other things aswell

--- Refs-and-portals/src/components/TimerChallenge.jsx
+++ Refs-and-portals/src/components/TimerChallenge.jsx
@@ -1,16 +1,40 @@
-export default function TimerChallenge({ title, targetTime }) {
+import { useRef, useState } from "react";
+
+function TimerChallenge({ title, targetTime }) {
+    const timer = useRef();
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpaired, setTimerExpaired] = useState(false);
+
+    function handleTimerStart(){
+        timer.current = setTimeout(() => {
+            setTimerExpaired(true);
+        }, targetTime * 1000)
+        setTimerStarted(true);
+    }
+
+    function handleTimerStop(){
+        clearTimeout(timer.current);
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpaired && <p>You lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 's' : ""}
             </p>
             <p>
-                <button>Start Challenge</button>
+                <button 
+                    onClick={timerStarted ? handleTimerStop : handleTimerStart}
+                >
+                    {timerStarted ? "Stop" : "Start"} Challenge
+                </button>
             </p>
-            <p>
-                
+            <p className={timerStarted ? "active" : null}>
+                {timerStarted ? "Timer is active" : "Timer is stopped"}
             </p>
         </section>
     )
 }
+
+export default TimerChallenge;
