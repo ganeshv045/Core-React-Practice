43f98f3a859a279ae0f6fa93b38f34d0aa38c657
ganeshv045 <ganesh045.v@gmail.com>
2024-09-15 20:28:20 +0530

    Introducing useEffect hook for handling side effects

--- SideEffects-useEffect-useCallback/src/App.jsx
+++ SideEffects-useEffect-useCallback/src/App.jsx
@@ -5,11 +5,21 @@ import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+import { useEffect } from 'react';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, [])
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -37,6 +47,9 @@ function App() {
     modal.current.close();
   }
 
+  console.log("availablePlaces", availablePlaces);
+
+
   return (
     <>
       <Modal ref={modal}>
@@ -63,7 +76,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
